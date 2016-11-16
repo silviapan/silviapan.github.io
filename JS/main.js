cc2a2b684f7d3ac9df78093e5a42cb762b4106b6
Silvia Pan <silvia.pan29@gmail.com>
2016-11-15 22:08:12 -0500

    update unless mobile

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,4 @@
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-  return '';
-}
-else {
+if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 $(function() {
 	var projects = $('.project-container');
 	var prevButtons = $('.prev');
